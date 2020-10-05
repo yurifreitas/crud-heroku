992fb1573a6453b5950aa229fa0302906a9d5dea
Yuri <yuribzfreitas@gmail.com>
2020-10-05 19:54:31 -0300

    ConnectDB

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const {Pool} = require('pg');
 const app = express();
 let port = process.env.PORT || 3000;
+let url = process.env.DATABASE_URL 
+
+const pool = new Pool({
+ connectionString: url,
+ ssl: {
+ rejectUnauthorized: false
+ }
+});
 
 app.get("/",(req,res) =>{
     res.send("Hello Word");
-
+    pool.connect();
 });
 
 app.listen(port,()=>{
